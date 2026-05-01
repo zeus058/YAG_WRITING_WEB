6f8a3dd69cdb3233b73d8f78ddd42a1d03609b1f
ynnhi2607 <huynhyennhi2607@gmail.com>
2026-05-01 17:27:14 +0700

    initial frontend

---
+++ yag/frontend/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-500">Test tailwind</h1>
+    </div>
+  );
+}

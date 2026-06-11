5c2858b4ed4cf88f703c44b6cae1b14accc2368e
rin <trangiahien058@gmail.com>
2026-06-11 12:10:08 +0700

    fix: database lưu truyện

--- src/backend/migrations/V7__story_commitments.sql
+++ src/backend/migrations/V7__story_commitments.sql
@@ -1,3 +1,3 @@
--- SQL Migration V4: Story Commitments
+-- SQL Migration V7: Story Commitments
 ALTER TABLE stories ADD COLUMN expected_chapters INTEGER NOT NULL DEFAULT 0;
 ALTER TABLE stories ADD COLUMN update_frequency VARCHAR(50) NOT NULL DEFAULT '1_week_1_chap';

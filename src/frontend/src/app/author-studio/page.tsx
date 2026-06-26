4e6aee86fcde7448190a45a9cf5ad0d72ccb9356
rin <trangiahien058@gmail.com>
2026-06-26 17:43:18 +0700

    fix cúi cùng

--- src/frontend/src/app/author-studio/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import { AuthorStudioScreen } from "@/components/features/author/AuthorScreens";
-
-export default function AuthorStudioPage() {
-  return <AuthorStudioScreen />;
-}
--- src/frontend/src/app/reports/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import { ReportsScreen } from "@/components/features/admin/AdminScreens";
-
-export default function ReportsPage() {
-  return <ReportsScreen />;
-}
